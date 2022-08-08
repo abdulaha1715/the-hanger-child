47b04bce3dc92ac9c91e0266474f4823792fc0d9
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-08-08 20:36:35 +0600

    display quantity selector plus minus

--- js/yoast-faq.js
+++ js/yoast-faq.js
@@ -1,23 +1,9 @@
-	jQuery(document).ready(function($){
-		var yoast = {
-			accordion: function(){
-				$('.wp-block-yoast-faq-block').find('.schema-faq-question').click(function(){
-					//Expand or collapse this panel
-					$(this).nextAll('.schema-faq-answer').eq(0).slideToggle('fast', function(){
-						if( $(this).hasClass('collapse') ){
-							$(this).removeClass('collapse');
-						}
-						else{
-							$(this).addClass('collapse');
-						}
-					});
-				
-					//Hide the other panels
-					$(".schema-faq-answer").not( $(this).nextAll('.schema-faq-answer').eq(0) ).slideUp('fast');
-				});
-
-				$('.wp-block-yoast-faq-block .schema-faq-question').click(function(){
-					$('.wp-block-yoast-faq-block .schema-faq-question').not( $(this) ).removeClass('collapse');
+jQuery(document).ready(function($){
+	var yoast = {
+		accordion: function(){
+			$('.wp-block-yoast-faq-block').find('.schema-faq-question').click(function(){
+				//Expand or collapse this panel
+				$(this).nextAll('.schema-faq-answer').eq(0).slideToggle('fast', function(){
 					if( $(this).hasClass('collapse') ){
 						$(this).removeClass('collapse');
 					}
@@ -25,8 +11,58 @@
 						$(this).addClass('collapse');
 					}
 				});
-			}
-		};
+			
+				//Hide the other panels
+				$(".schema-faq-answer").not( $(this).nextAll('.schema-faq-answer').eq(0) ).slideUp('fast');
+			});
 
-		yoast.accordion();
-	});
+			$('.wp-block-yoast-faq-block .schema-faq-question').click(function(){
+				$('.wp-block-yoast-faq-block .schema-faq-question').not( $(this) ).removeClass('collapse');
+				if( $(this).hasClass('collapse') ){
+					$(this).removeClass('collapse');
+				}
+				else{
+					$(this).addClass('collapse');
+				}
+			});
+		}
+	};
+
+	yoast.accordion();
+
+	// Cart page quantity plus and minus icon
+	jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><i class="thehanger-icons-ui_plus-window"></i></div><div class="quantity-button quantity-down"><i class="thehanger-icons-ui_minus-window"></i></div></div>').insertAfter('.quantity input');
+
+	// Cart page quantity plus and minus icon click event
+    jQuery('.quantity').each(function() {
+		var spinner = jQuery(this),
+		input       = spinner.find('input[type="number"]'),
+		btnUp       = spinner.find('.quantity-up'),
+		btnDown     = spinner.find('.quantity-down'),
+		min         = input.attr('min'),
+		max         = input.attr('max');
+
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+    });	 
+});
