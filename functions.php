b06d48fd087773c92bebb4d6c28064b7b4c8bf86
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-08-10 00:00:49 +0600

    filter name changed

--- functions.php
+++ functions.php
@@ -278,6 +278,4 @@ function isa_woo_cart_attribute_values( $cart_item, $cart_item_key ) {
     echo $out.= ', </span>';
 }
 
-if( is_page( 'cart' ) ) {
-    add_filter( 'woocommerce_cart_item_name', 'isa_woo_cart_attribute_values', 10, 2 );
-}
+add_filter( 'woocommerce_cart_item_name', 'isa_woo_cart_attribute_values', 10, 2 );
