052976c921aff303102bdf83ccf4452bc904b885
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-08-17 02:21:56 +0600

    Page as post

--- pageaspost.php
+++ pageaspost.php
@@ -14,7 +14,7 @@
 
 									if ( function_exists('yoast_breadcrumb') ) {
 
-										yoast_breadcrumb( '</p><p id=“breadcrumbs”>','</p><p>' );
+										yoast_breadcrumb( '<p id=breadcrumbs>','</p>' );
 
 									}
 
